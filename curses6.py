bf93d94d45bf90a290142f028e9d3d092cb027dd
raviteja1452 <b.raviteja1452@gmail.com>
2016-08-24 23:45:14 +0530

    removing of pointer

--- curses6.py
+++ curses6.py
@@ -14,15 +14,20 @@ Horizontal = 1
 while q != ord('q'):
 	screen.clear()
 	screen.addstr(y,x,'Hello World!')
+	screen.move(dims[0] - 1,dims[1] - 1)
 	screen.refresh()
 	q = screen.getch()
 	if q == ord('w') and y > 0 :
 		y -= 1
-	elif q == ord('s') and y < dims[0] -1 :
+	elif q == ord('s') and y < dims[0] - 1 :
 		y += 1
 	elif q == ord('a') and x > 0:
 		x -= 1
-	elif q == ord('d') and x == dims[1] - len('Hello World') - 1:
+	elif q == ord('d') and x < dims[1] - len('Hello World') - 1:
 		x += 1
-	time.sleep(0.05)
+	if y == dims[0] - 1 and x == dims[1] - len('Hello World!'):
+		if q == ord('s'):
+			y -= 1
+		elif q == ord('d'):
+			x -= 1
 curses.endwin()
