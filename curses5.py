6c3689163c439a3bdaef1e10ddc3538bcef54edb
raviteja1452 <b.raviteja1452@gmail.com>
2016-08-24 23:11:11 +0530

    user input from curses5

---
+++ curses5.py
@@ -0,0 +1,28 @@
+# Taking User Input using curses
+# magic
+# RationalScripts
+
+import curses
+import time
+screen = curses.initscr()
+curses.noecho()
+dims = screen.getmaxyx()
+q = -1
+x,y =0,0
+Vertical = 1
+Horizontal = 1
+while q != ord('q'):
+	screen.clear()
+	screen.addstr(y,x,'Hello World!')
+	screen.refresh()
+	q = screen.getch()
+	if q == ord('w') and y > 0 :
+		y -= 1
+	elif q == ord('s') and y < dims[0] -1 :
+		y += 1
+	elif q == ord('a') and x > 0:
+		x -= 1
+	elif q == ord('d') and x < dims[1] - len('Hello World') - 1:
+		x += 1
+	time.sleep(0.05)
+curses.endwin()
